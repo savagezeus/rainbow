da0ec4086066a94994f30929bb3fdfbca6290c47
savagezeus <52446818+savagezeus@users.noreply.github.com>
2019-07-20 22:03:26 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ client.on("ready", () => {
 
   console.log(`Logged in as ${client.user.username}!`);
 
-  client.user.setActivity('BEACUSE ZEUS IS DEAD FROM INSIDE', {type: "crying"})
+  client.user.setActivity('ZEUS IS DEAD FROM INSIDE', {type: "watching"})
 console.log(`${client.user.tag} running on ${client.guilds.size} guilds with ${client.users.size} users.`);
-});
+})
  
 client.on("message", async (message) => {
 	
