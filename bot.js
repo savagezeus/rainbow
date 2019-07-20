b0387089225218caaf625629bbf2605f765abc22
savagezeus <52446818+savagezeus@users.noreply.github.com>
2019-07-20 21:58:55 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
 
   console.log(`Logged in as ${client.user.username}!`);
 
-  client.user.setActivity('ZEUS IS DEAD HELP HIM 😫', {type: "streaming"})
+  client.user.setActivity('BEACUSE ZEUS IS DEAD FROM INSIDE', {type: "CRYING"})
 console.log(`${client.user.tag} running on ${client.guilds.size} guilds with ${client.users.size} users.`);
 });
  
