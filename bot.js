520b1d95a1c3f0c08b5da2b09bf5b8cfd3435008
savagezeus <52446818+savagezeus@users.noreply.github.com>
2019-07-22 19:21:17 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
 
   console.log(`Logged in as ${client.user.username}!`);
 
-  client.user.setActivity('SERVER UNDER MAINTENANCE', {type: "streaming"})
+  client.user.setActivity('NEW SERVER THEME IS HERE', {type: "streaming"})
 console.log(`${client.user.tag} running on ${client.guilds.size} guilds with ${client.users.size} users.`);
 })
  
